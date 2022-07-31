271ea4967db6f4d9d4ae37e53e21ca3e6f26ec85
gcm <1453400328@qq.com>
2022-07-31 17:34:20 +0800

    ...

--- gcm/mysql/index.php
+++ gcm/mysql/index.php
@@ -17,16 +17,36 @@
     echo "连接成功";
     $sql = "SELECT id, name FROM stu";
     $result = $conn->query($sql);
-    print("\n");
+    echo "<br>";
     print_r($result);
 
     if ($result->num_rows > 0) {
         // 输出数据
         while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"] . " - Name: " . $row["name"] . "\n";
+            echo "<br>id: " . $row["id"] . " - name: " . $row["name"];
         }
     } else {
         echo "0 结果";
     }
+    // 预处理及绑定
+    $stmt = $conn->prepare("INSERT INTO stu(id,name) VALUES (?, ?)");
+    
+    $stmt->bind_param("is", $id, $name);
+
+    // 设置参数并执行
+    $id = null;
+    $name = "g1";
+    $stmt->execute();
+
+    $name = "g2";
+    $stmt->execute();
+
+    $name = "g3";
+
+    $stmt->execute();
+
+    echo "新记录插入成功";
+
+    $stmt->close();
     $conn->close();
     ?>
